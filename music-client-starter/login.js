026526d14dd7b821efe6d35a35b93a9912624231
Antonedei Wonimidei Otoro <otoroj@engineer.com>
2022-11-17 14:32:41 -0600

    config files

--- music-client-starter/login.js
+++ music-client-starter/login.js
@@ -1,29 +1,29 @@
-window.onload = function(){
-    document.getElementById('loginBtn').onclick = login;
-}
+window.onload = function () {
+  document.getElementById("loginBtn").onclick = login;
+};
 
-async function login(){
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+async function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    const response = await fetch('http://localhost:3000/api/auth/login', {
-        method: 'POST',
-        body: JSON.stringify({
-            username,
-            password
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+  const response = await fetch("http://localhost:3000/api/auth/login", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if(response.ok){
-        const result = await response.json();
-        sessionStorage.setItem('token', result.accessToken);
-        sessionStorage.setItem('username', result.username);
-        location.href = 'welcome.html';
-    } else {
-        document.getElementById('errorMsg').innerText = 'Incorrect username and password';
-    }
-   
+  if (response.ok) {
+    const result = await response.json();
+    sessionStorage.setItem("token", result.accessToken);
+    sessionStorage.setItem("username", result.username);
+    location.href = "welcome.html";
+  } else {
+    document.getElementById("errorMsg").innerText =
+      "Incorrect username and password";
+  }
 }

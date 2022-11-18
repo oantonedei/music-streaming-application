6194251078a6e26853446c34b5de913473f410be
Jean <damour2014stage@gmail.com>
2022-11-18 13:23:10 -0600

    adding music player

--- music-client-starter/welcome.js
+++ music-client-starter/welcome.js
@@ -1,50 +1,46 @@
-window.onload = function(){
-    init();
-    document.getElementById('logoutBtn').onclick = logout;
-    fetchAllSongs();
+window.onload = function () {
+  init();
+  document.getElementById("logoutBtn").onclick = logout;
+  document.getElementById("btn").onclick = buttonIcon;
+  fetchAllSongs();
+};
+
+function init() {
+  document.getElementById("username").innerText = sessionStorage.getItem("username");
 }
 
-function init(){
-    document.getElementById('username').innerText = sessionStorage.getItem('username');
+function logout() {
+  sessionStorage.removeItem("username");
+  sessionStorage.removeItem("token");
+  location.href = "login.html";
 }
 
-function logout(){
-    sessionStorage.removeItem('username');
-    sessionStorage.removeItem('token');
-    location.href= 'login.html';
+function fetchAllSongs() {
+  fetch("http://localhost:3000/api/music", {
+    headers: {
+      Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((songs) => console.log(songs));
 }
 
-function fetchAllSongs(){
-    fetch('http://localhost:3000/api/music', {
-        headers: {
-            'Authorization': `Bearer ${sessionStorage.getItem('token')}`
-        }
-    })
-    .then(response => response.json())
-    .then(songs => console.log(songs));
-
-    let html = `
-    <tr>
-    <th>ID</th>
-    <th>TITLE</th>
-    <th>RELEASE DATE</th>
-    <th>ACTION</th>
-
-    </tr>
-    `;
-
-    response.forEach(element => {
-        html+=`
-        <tr>
-        <td>${element.id}</td>
-        <td>${element.title}</td>
-        <td>${element.releaseDate}</td>
-        <td><button>+</button></td>
-    
-        </tr>
-        `
-        
-    });
-    // document.getElementById('')
-        
+function buttonIcon() {
+  let shuffle = document.getElementById("shuffle");
+  let repeat = document.getElementById("repeat");
+  let repeatAll = document.getElementById("repeatAll");
+    console.log(shuffle);
+  if (shuffle.style.display === "block") {
+    repeat.style.display = "block";
+    repeatAll.style.display = "none";
+    shuffle.style.display = "none";
+  } else if (repeat.style.display === "block") {
+    shuffle.style.display = "none";
+    repeatAll.style.display = "block";
+    repeat.style.display = "none";
+  } else if (repeatAll.style.display === "block") {
+    shuffle.style.display = "block";
+    repeat.style.display = "none";
+    repeatAll.style.display = "none";
+  }
 }
